e3248bb5fd9adb512ff58b244e55d4a9b5ae9051
Juanma <martinjm174@gmail.com>
2025-10-25 22:19:22 -0300

    fix extractor agent

--- src/agents/extractor_assistant.py
+++ src/agents/extractor_assistant.py
@@ -9,21 +9,20 @@ class Person(BaseModel):
     name: str= Field(description="The name of the person")
     age: str= Field(description="The age of the person")
 
-
-
+class State(MessagesState):
+    name: str
+    age: str
 
 def chat_node(state: MessagesState):
     sys_msg = SystemMessage(content="Eres un asistente especializado para extraer los datos de una persona de una conversacion")
     ia_message = model_with_tools.invoke([sys_msg] + [state["messages"][-1]])
     print(ia_message)
-    return {"messages": [ia_message]}
-
-
+    return {"name": ia_message.name, "age": ia_message.age}
 
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 model_with_tools = model.with_structured_output(schema=Person)
 
-graph = StateGraph(MessagesState)
+graph = StateGraph(State)
 graph.add_node("chat", chat_node)
 graph.add_edge(START, "chat")
 graph.add_edge("chat", END)
