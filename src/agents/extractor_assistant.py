9e69b7531d884afcdd2605811ef9ee8fae6776e8
Juanma <martinjm174@gmail.com>
2025-10-25 22:12:10 -0300

    add extractor agent

--- src/agents/extractor_assistant.py
+++ src/agents/extractor_assistant.py
@@ -2,23 +2,30 @@ from langgraph.constants import START, END
 from langgraph.graph import StateGraph, MessagesState
 from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+class Person(BaseModel):
+    """ Personal information """
+    name: str= Field(description="The name of the person")
+    age: str= Field(description="The age of the person")
+
+
 
 
 def chat_node(state: MessagesState):
-    sys_msg = SystemMessage(content="Eres un asistente especializado en estadisticas de futbol")
-    print(f"estado actual: {state}")
+    sys_msg = SystemMessage(content="Eres un asistente especializado para extraer los datos de una persona de una conversacion")
     ia_message = model_with_tools.invoke([sys_msg] + [state["messages"][-1]])
+    print(ia_message)
     return {"messages": [ia_message]}
 
 
-VECTOR_STORE_IDS = ["vs_68fcf3fc0ee4819187471dd2e1e1ca75"]
-TOOLS = [{"type": "file_search", "vector_store_ids": VECTOR_STORE_IDS}]
 
 model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-model_with_tools = model.bind_tools(TOOLS)
+model_with_tools = model.with_structured_output(schema=Person)
 
 graph = StateGraph(MessagesState)
 graph.add_node("chat", chat_node)
 graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
 
 app = graph.compile()
--- src/agents/repository_assistant_v2.py
+++ src/agents/repository_assistant_v2.py
@@ -19,7 +19,7 @@ def feedback():
         exit()
 
 
-def chat_node(state: MessagesState) -> dict:
+def chat_node(state: MessagesState):
     sys_msg = SystemMessage(content="Eres un asistente especializado en dependencias de Java."
                                     + "Tu función es ayudar al usuario a encontrar la información más reciente sobre artefactos publicados en Maven Central, como la última versión disponible, la fecha de publicación y las ramas activas."
                                     + "Responde siempre en español, de manera clara, técnica y estructurada.")
